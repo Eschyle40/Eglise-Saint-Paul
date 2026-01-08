a226f0c494b390f8b4e291fc0fcda27bceb6a2ee
Eschyle40 <m64yh8qwd7@privaterelay.appleid.com>
2026-01-08 17:01:59 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@
  */
 
 export default {
-    base : process.env.NODE_ENV === 'production' ? '/my-app/' : '/',
+    base : process.env.NODE_ENV === 'production' ? '/Eglise-Saint-Paul/' : '',
     // other Vite configuration options can go here
 };
+
